460eaca06a490f6950daf39fa6df87641eb90778
einnarth <113421210+einnarth@users.noreply.github.com>
2022-11-22 08:38:30 +0100

    Changet output in line 9 to "Hello World"

--- oop/Program.cs
+++ oop/Program.cs
@@ -6,7 +6,7 @@ namespace oop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ahoj Svet ");
+            Console.WriteLine("Hello World ");
         }
     }
 }
