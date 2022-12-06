277deab54435c8c2538275214a4ec275815ef3be
einnarth <113421210+einnarth@users.noreply.github.com>
2022-12-06 15:21:29 +0100

    Edited Main method and class Book (10.3)

--- oop/Book.cs
+++ oop/Book.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+
 
 namespace oop
 {
@@ -8,42 +10,58 @@ namespace oop
         private int _pages;
         private string _category;
         private string _author;
-        private string _relaseDate;
+        private int _relaseDate;
+        
 
-        public void TitleM(string tit)
+        public string TitleM
         {
-            _title = tit;
+            get => _title;
+            set => _title = value;
         }
 
-        public void PagesM(int page)
+        public int PagesM
         {
-            _pages = page;
+            get => _pages;
+            set => _pages = value < 0 ? 1 : value;
         }
 
-        public void CategoryM(string cat)
+        public string CategoryM
         {
-            _category = cat;
+            get => _category;
+            set => _category = value;
+
         }
 
-        public void AuthorM(string aut)
+        public string AuthorM
         {
-            _author = aut;
+            get => _author;
+           set => _author = value;
         }
 
-        public void RelaseDateM(string relase)
+        public int RelaseDateM
         {
-            _relaseDate = relase;
+            get => _relaseDate;
+            set
+            {
+                if (Enumerable.Range(1450, 2021).Contains(value))
+                {
+                    _relaseDate = value;
+                }
+                else
+                {
+                    _relaseDate = -1;
+                }
+            }
         }
+
 
         public void Information()
         {
-            Console.WriteLine("Názov: " + _title);
-            Console.WriteLine("Počet strán " + _pages);
-            Console.WriteLine("Kategória " + _category);
-            Console.WriteLine("Autor: " + _author);
-            Console.WriteLine("Dátum vydania " + _relaseDate);
-            
+            Console.WriteLine($"Nazov: {_title}");
+            Console.WriteLine($"Pocet stran { _pages}");
+            Console.WriteLine($"Kategória {_category}");
+            Console.WriteLine($"Autor: {_author}");
+            Console.WriteLine($"Dátum vydania {_relaseDate}");
         }
-     
     }
 }
--- oop/Program.cs
+++ oop/Program.cs
@@ -1,19 +1,18 @@
 ﻿
-using System;
-using Microsoft.VisualBasic;
-
 namespace oop
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Book lotr = new Book();
-            lotr.TitleM("miskociganko"); 
-            lotr.PagesM(153);
-            lotr.CategoryM("fantasy");
-            lotr.AuthorM("michal bernat");
-            lotr.RelaseDateM("06/06/2012");
+            Book lotr = new Book
+            {
+                TitleM = "miskociganko",
+                PagesM = 153,
+                CategoryM = "fantasy",
+                AuthorM = "michal bernat",
+                RelaseDateM = 2012
+            };
             lotr.Information();
             
             
