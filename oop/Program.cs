8e84db6defb1854472194d98f6daef81e11ea473
einnarth <113421210+einnarth@users.noreply.github.com>
2022-12-06 14:20:08 +0100

    Updated main method (10.2)

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using Microsoft.VisualBasic;
 
 namespace oop
 {
@@ -6,10 +8,16 @@ namespace oop
     {
         static void Main(string[] args)
         {
-            Book LOTR = new Book();
-            LOTR.title = "miskociganko";
-            Console.WriteLine(LOTR.title);
+            Book lotr = new Book();
+            lotr.TitleM("miskociganko"); 
+            lotr.PagesM(153);
+            lotr.CategoryM("fantasy");
+            lotr.AuthorM("michal bernat");
+            lotr.RelaseDateM("06/06/2012");
+            lotr.Information();
+            
             
         }
+        
     }
 }
