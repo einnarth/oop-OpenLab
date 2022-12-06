6826966e89ba7d1a7eced990f3d8d7bc1a82ac8d
einnarth <113421210+einnarth@users.noreply.github.com>
2022-12-06 14:19:58 +0100

    Updated class book with new informations

--- oop/Book.cs
+++ oop/Book.cs
@@ -1,9 +1,49 @@
+using System;
+
 namespace oop
 {
     public class Book
     {
-        public string title;
-        private int pages;
-        public Book(){}
+        private string _title;
+        private int _pages;
+        private string _category;
+        private string _author;
+        private string _relaseDate;
+
+        public void TitleM(string tit)
+        {
+            _title = tit;
+        }
+
+        public void PagesM(int page)
+        {
+            _pages = page;
+        }
+
+        public void CategoryM(string cat)
+        {
+            _category = cat;
+        }
+
+        public void AuthorM(string aut)
+        {
+            _author = aut;
+        }
+
+        public void RelaseDateM(string relase)
+        {
+            _relaseDate = relase;
+        }
+
+        public void Information()
+        {
+            Console.WriteLine("Názov: " + _title);
+            Console.WriteLine("Počet strán " + _pages);
+            Console.WriteLine("Kategória " + _category);
+            Console.WriteLine("Autor: " + _author);
+            Console.WriteLine("Dátum vydania " + _relaseDate);
+            
+        }
+     
     }
 }
